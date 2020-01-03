8f6003f328e0482bff9052843063bbea1edb7f68
gerjanvdbosch <gerjanvdbosch@users.noreply.github.com>
2020-01-03 20:10:08 +0000

    Automated deployment: Fri Jan  3 20:10:08 UTC 2020 cf4410f7b73ec65b76ab8cb5aac6ab74f34eeabd

---
+++ precache-manifest.abfdba8b43edb71271ddc906395e8105.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657c169d252721b1c5fa",
+    "url": "/css/chunk-vendors.1d6082ea.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4233c03711143f3cffb7dfa6701657fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423612873baa65ef9d6d",
+    "url": "/js/about.14c66757.js"
+  },
+  {
+    "revision": "cdbef4b3cf3dfb9bab0d",
+    "url": "/js/app.ba5aef46.js"
+  },
+  {
+    "revision": "657c169d252721b1c5fa",
+    "url": "/js/chunk-vendors.ad668bec.js"
+  },
+  {
+    "revision": "d83ded437438d5e93793eb604ae717d4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
