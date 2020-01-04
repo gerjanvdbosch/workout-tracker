329be91ffbebe37cf7b1f15727b236b052059b0b
Ger Jan van den Bosch <g.j.van.den.bosch@connectingthedots.nl>
2020-01-04 22:18:29 +0100

    Added icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,7 +2,12 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'md',
+  },
 });
