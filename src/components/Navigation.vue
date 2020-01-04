c641d5afd9a32cc5f19fa805bcb5f54c79763fc7
Ger Jan van den Bosch <g.j.van.den.bosch@connectingthedots.nl>
2020-01-04 20:34:15 +0100

    Added navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    flat
+    tile
+  >
+    <v-toolbar
+      color="primary"
+      dark
+      elevation="1"
+    >
+      <v-toolbar-title>Workout Log</v-toolbar-title>
+
+      <template v-slot:extension>
+        <v-tabs
+          background-color="grey lighten-3"
+          light
+        >
+          <v-tab to="/">Home</v-tab>
+          <v-tab to="/about">About</v-tab>
+        </v-tabs>
+
+      </template>
+    </v-toolbar>
+
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Navigation'
+});
+</script>
