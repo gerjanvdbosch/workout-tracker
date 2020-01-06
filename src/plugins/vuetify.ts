a1ab213668047d05eb2bcd6d393c2820d8b61e16
Ger Jan van den Bosch <g.j.van.den.bosch@connectingthedots.nl>
2020-01-06 21:42:13 +0100

    Fixed node types

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import VueMoment from 'vue-moment';
 
+import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify);
-Vue.use(require('vue-moment'));
+Vue.use(VueMoment);
 
 export default new Vuetify({
   icons: {
