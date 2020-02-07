a1ab0d94f2fa73335548bc773e372e40dba4980c
Ger Jan van den Bosch <g.j.van.den.bosch@connectingthedots.nl>
2020-02-07 20:30:13 +0100

    Updated icons

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,11 @@ module.exports = {
           'src': './img/icons/android-chrome-512x512.png',
           'sizes': '512x512',
           'type': 'image/png'
+        },
+        {
+          'src': './img/icons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png'
         }
       ]
     }
