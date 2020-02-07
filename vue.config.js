a52fabc71ee8adfd01c887aa54b113b6d0e558cb
Ger Jan van den Bosch <g.j.van.den.bosch@connectingthedots.nl>
2020-02-07 22:17:30 +0100

    Updated icons

--- vue.config.js
+++ vue.config.js
@@ -9,19 +9,7 @@ module.exports = {
     name: 'Workout Log',
     themeColor: '#1976D2',
     manifestOptions: {
-      background_color: '#1976D2',
-      icons: [
-        {
-          'src': './img/icons/android-chrome-512x512.png',
-          'sizes': '512x512',
-          'type': 'image/png'
-        },
-        {
-          'src': './img/icons/android-chrome-144x144.png',
-          'sizes': '144x144',
-          'type': 'image/png'
-        }
-      ]
+      background_color: '#1976D2'
     }
   }
 };
